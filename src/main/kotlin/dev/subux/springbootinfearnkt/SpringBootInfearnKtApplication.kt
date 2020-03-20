6deab91a3d9839ec98c4881441ddd8bd8d1bdd61
Subin Lee <chandar.sblee@gmail.com>
2020-03-20 11:06:24 +0900

    Init

---
+++ src/main/kotlin/dev/subux/springbootinfearnkt/SpringBootInfearnKtApplication.kt
@@ -0,0 +1,11 @@
+package dev.subux.springbootinfearnkt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootInfearnKtApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootInfearnKtApplication>(*args)
+}
---
+++ src/test/kotlin/dev/subux/springbootinfearnkt/SpringBootInfearnKtApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.subux.springbootinfearnkt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootInfearnKtApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
