80f94a5977fcb7268b0ec9e3fff197275e1c4db4
Subin Lee <chandar.sblee@gmail.com>
2020-03-24 11:28:54 +0900

    Add CORS config, hello URI

--- src/main/kotlin/dev/subux/springbootinfearnkt/Hello.kt
+++
@@ -1,7 +0,0 @@
-package dev.subux.springbootinfearnkt
-
-data class Hello(var prefix: String, var name: String) {
-    override fun toString(): String {
-        return prefix + " " + name
-    }
-}
