72a549f7fe4af68ded9de441dfef686a40dc68b0
Subin Lee <chandar.sblee@gmail.com>
2020-03-24 16:15:58 +0900

    Add JDBC, PgSQL dependency, Executable queries

--- src/main/kotlin/dev/subux/springbootinfearnkt/PgSQLRunner.kt
+++ src/main/kotlin/dev/subux/springbootinfearnkt/PgSQLRunner.kt
@@ -9,20 +9,21 @@ import java.sql.Statement
 import javax.sql.DataSource
 
 @Component
-class H2Runner(
+class PgSQLRunner(
     private val dataSource: DataSource,
     private val  jdbcTemplate: JdbcTemplate
 ) : ApplicationRunner {
     override fun run(args: ApplicationArguments?) {
         val conn: Connection = dataSource.getConnection()
         val stmt: Statement = conn.createStatement()
-        val sql = "CREATE TABLE USER(ID INTEGER NOT NULL, name VARCHAR(255), PRIMARY KEY (ID))"
+        val sql = "CREATE TABLE account(ID INTEGER NOT NULL, name VARCHAR(255), PRIMARY KEY (ID))"
         stmt.executeUpdate(sql)
         stmt.close()
 
+        println(dataSource.javaClass)
         println(conn.metaData.userName)
         println(conn.metaData.url)
 
-        jdbcTemplate.execute("INSERT INTO USER VALUES (1, 'subin')")
+        jdbcTemplate.execute("INSERT INTO account VALUES (1, 'subin')")
     }
 }
